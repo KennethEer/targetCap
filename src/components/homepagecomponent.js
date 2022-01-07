488073acc1b7fe2dcdeda593a3d6a273fb5099f4
KennethEer <59533849+KennethEer@users.noreply.github.com>
2022-01-08 00:52:08 +0800

    Edit homepage UI

--- src/components/homepagecomponent.js
+++ src/components/homepagecomponent.js
@@ -1,5 +1,7 @@
 import React, { Component }from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 export default class homepage extends Component {
@@ -15,9 +17,14 @@ export default class homepage extends Component {
                     to achieve your dream CAP and set your target grades
                 </h4>
                 <p className="subsubheading"></p>
-                <a href="/quick" className="btn btn-info" role="button" style={{marginRight: '40px'}}>Quick</a>
-
-                <a href="/advanced" className="btn btn-info" role="button">Advanced</a>
+                <Row className="justify-content-md-center">
+                <Col xs lg="4"><img style={{width:"200px", height:"200px", }}src="/checklist.png" alt="" /><br /><br />
+                <a href="/quick" className="btn btn-info" role="button" style={{fontSize:"20px"}}>Quick&#10132;</a><br /><br />
+                </Col>
+                <Col xs lg="4"><img style={{width:"300px", height:"200px"}}src="/nus.png" alt="" /><br /><br />
+                <a href="/advanced" className="btn btn-info" role="button" style={{fontSize:"20px"}}>Advanced&#10132;</a><br /><br />
+                </Col>
+                </Row>
                 </Container>
             </div>
         )
